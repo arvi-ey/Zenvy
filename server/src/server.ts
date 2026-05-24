1bf7fac8728cf82d6c35134056e381580d7a833d
arvi-ey <tuhinroyand@gmail.com>
2026-05-24 22:14:45 +0530

    chore:
    Error handeling set-up completed , working on session db

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,14 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import globalErrorHandler from "./middlewares/error.middleware.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
 const app = express();
 
+app.use(cookieParser());
 app.use(cors());
 app.use(express.json());
 
@@ -14,6 +17,11 @@ const PORT = process.env.PORT || 5000;
 app.get("/", (req, res) => {
     res.send("Server is running...");
 });
+
+
+
+
+app.use(globalErrorHandler)
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
